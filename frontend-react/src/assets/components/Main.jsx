d127a533c45be9fafabd4ddab578b41438d3403a
DonFidelis <fdchulu2@gmail.com>
2025-09-15 14:43:26 +0200

    Authentication functionalities

--- frontend-react/src/assets/components/Main.jsx
+++ frontend-react/src/assets/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
             <div className='p-5 text-center bg-light-dark rounded'>
                 <h1 className='text-light'>Stock Prediction Portal</h1>
                 <p className='text-light'>This stock prediction application utilizes machine learning techniques, specifically employing Keras, and LSTM model, intergrated within the Django framework. it forecasts future stock prices by analyzing 100-days and 200-days moving averages. essential indicators widely used by stock analysts to inform trading and investment decisions. </p>
-                <Button text='Login' class='btn-info' />
+                <Button text='Explore Now' class='btn-info' url="/dashboard" />
             </div>
         </div>
 
